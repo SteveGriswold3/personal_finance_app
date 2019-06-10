d8c820e6450d3721d666d1fb0a9a71f38d90ebc9
Steve Griswold <steve.griswold@usfoods.com>
2019-06-09 22:33:45 -0700

    add unit test and integration tests initial commit.

---
+++ test/unittest.py
@@ -0,0 +1,16 @@
+"""Always Test and Test Always."""
+
+import unittest as unittest
+
+class TestAccounts(unittest.TestCase):
+    def test_account_add(self):
+        # TODO: write account add test.
+        pass
+
+    def test_account_get_balance(self):
+        # TODO: write account return balance test.
+        pass
+
+    def test_remove_account(self):
+        # TODO: write remove account test.
+        pass
